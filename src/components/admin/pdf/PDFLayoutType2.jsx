690dab5cae9eca7fceda0c448514aa888397c530
SM @ KO <ai3@ko.in.th>
2025-12-17 07:00:56 +0000

    File changes

--- src/components/admin/pdf/PDFLayoutType2.jsx
+++ src/components/admin/pdf/PDFLayoutType2.jsx
@@ -187,8 +187,8 @@ export default function PDFLayoutType2({ applicant }) {
                     </div>
 
                     {/* Photo Box */}
-                    <div className="col-span-2">
-                        <div className="border border-slate-400 w-[50px] h-[66px] flex items-center justify-center bg-slate-50 relative mx-auto">
+                    <div className="col-span-3 flex justify-end">
+                        <div className="border border-slate-400 w-[100px] h-[130px] flex items-center justify-center bg-slate-50 relative">
                              {applicant.photo_url ? (
                                 <img src={applicant.photo_url} alt="Photo" className="w-full h-full object-cover absolute inset-0" />
                             ) : (
