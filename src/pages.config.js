aae3c0f996dbd61dc6304f4dbd598edbba047407
SM @ KO <ai3@ko.in.th>
2025-12-24 02:15:51 +0000

    Update PDF Layout

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,7 +1,6 @@
 import Admin from './pages/Admin';
 import Application from './pages/Application';
 import Home from './pages/Home';
-import PDFPreview from './pages/PDFPreview';
 import __Layout from './Layout.jsx';
 
 
@@ -9,7 +8,6 @@ export const PAGES = {
     "Admin": Admin,
     "Application": Application,
     "Home": Home,
-    "PDFPreview": PDFPreview,
 }
 
 export const pagesConfig = {
