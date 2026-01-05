c00daa011cf2f2f741f17eb8307713117e032ad4
SM @ KO <ai3@ko.in.th>
2026-01-05 08:49:30 +0000

    เอกสารหนังสือยินยอม

--- src/pages.config.js
+++ src/pages.config.js
@@ -10,6 +10,7 @@ import pdpaForm from './pages/pdpa-form';
 import userLogin from './pages/user-login';
 import fmHrd30 from './pages/fm-hrd-30';
 import userDashboard from './pages/user-dashboard';
+import salaryDeductionForm from './pages/salary-deduction-form';
 import __Layout from './Layout.jsx';
 
 
@@ -26,6 +27,7 @@ export const PAGES = {
     "user-login": userLogin,
     "fm-hrd-30": fmHrd30,
     "user-dashboard": userDashboard,
+    "salary-deduction-form": salaryDeductionForm,
 }
 
 export const pagesConfig = {
