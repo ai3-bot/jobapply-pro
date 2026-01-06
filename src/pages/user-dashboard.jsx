1a352fbaff14241384fb99ea134980dcb10e8021
SM @ KO <ai3@ko.in.th>
2026-01-06 07:04:54 +0000

    File changes

--- src/pages/user-dashboard.jsx
+++ src/pages/user-dashboard.jsx
@@ -63,7 +63,7 @@ export default function UserDashboard() {
         { id: 5, name: 'FM-HRD-30', description: 'การตรวจประวัติอาชญากรรม', link: '/fm-hrd-30' },
         { id: 6, name: 'หนังสือมอบอำนาจในการตรวจประวัติอาชญากรรม', description: 'หนังสือมอบอำนาจตรวจประวัติ', link: '/criminal-check-form' },
         { id: 7, name: 'แบบสปส.', description: 'แบบฟอร์มประกันสังคม', link: null },
-        { id: 8, name: 'ใบขอเอาประกัน', description: 'แบบฟอร์มประกันสังคม', link: null }
+        { id: 8, name: 'ใบขอเอาประกัน', description: 'แบบฟอร์มประกันภัยพนักงาน', link: '/insurance-enrollment' }
     ];
 
     return (
