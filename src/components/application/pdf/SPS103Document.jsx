53088adc4e1a25c99dc37204d520859c8c534718
SM @ KO <ai3@ko.in.th>
2026-01-08 07:16:00 +0000

    Manual code change

--- src/components/application/pdf/SPS103Document.jsx
+++ src/components/application/pdf/SPS103Document.jsx
@@ -348,8 +348,8 @@ export default function SPS103Document({ applicant, formData = {} }) {
                             <div className="bordor-t"></div>
                             {/* ลายเซ็นนายจ้าง */}
                             <div className="mt-1">
-                                <p className="mb-1">ข้าพเจ้าขอรับรองว่าข้อความดังกล่าวข้างต้น ถูกต้องตามความเป็นจริงทุกประการ</p>
-                                <div className="flex gap-4">
+                                <p className="text-center border-t border-black">ข้าพเจ้าขอรับรองว่าข้อความดังกล่าวข้างต้น ถูกต้องตามความเป็นจริงทุกประการ</p>
+                                <div className="flex gap-4 px-5 py-2 text-center">
                                     <div className="flex justify-center">
                                         <div className="w-[80px] h-[80px] rounded-full border-2 border-slate-300 flex items-center justify-center text-center">
                                             <div>
@@ -359,7 +359,7 @@ export default function SPS103Document({ applicant, formData = {} }) {
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="flex justify-end">
+                                    <div className="flex justify-end space-y-1">
                                         <div className="text-center">
                                             <div className="mb-1">ลงชื่อ<div className="inline-block border-b border-dotted border-slate-400 w-48 mx-2 min-h-[20px]"></div>นายจ้าง</div>
                                             <div className="mb-1">(<div className="inline-block border-b border-dotted border-slate-400 w-48 min-h-[20px]"></div>)</div>
