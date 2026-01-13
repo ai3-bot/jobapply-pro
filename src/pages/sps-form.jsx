2ecf4a166cd58388bf93f106f2ef3e6350bc4fb7
SM @ KO <ai3@ko.in.th>
2026-01-13 06:13:21 +0000

    Fix unicode entity

--- src/pages/sps-form.jsx
+++ src/pages/sps-form.jsx
@@ -33,13 +33,6 @@ export default function SPSFormPage() {
         salary: '',
         signatureDate: '',
         
-        // For foreigners (สำหรับคนต่างด้าว)
-        passportNumber: '',
-        workPermitNumber: '',
-        otherDocument: '',
-        otherDocumentNumber: '',
-        workPermitNumber2: '',
-        
         // For SPS 9-02
         educationLevel: '',
         educationMajor: ''
